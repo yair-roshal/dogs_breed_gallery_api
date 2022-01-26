e590015d2431f40bcf44a9ee822ada4818472cbb
[yair-r] <yair7705778@gmail.com>
2022-01-26 20:40:05 +0300

    axios

--- src/FetchData.js
+++ src/FetchData.js
@@ -5,44 +5,79 @@ import FetchImg from "./FetchImg"
 
 export const FetchData = () => {
   const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
+  // const [data, setData] = useState([])
   const [dogsArr, setDogsArr] = useState([])
 
   const URL = "https://dog.ceo/api/breeds/list/all"
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      try {
-        const { data: response } = await axios.get(URL)
-          setData(response)
-          setDogsArr(Object.keys(data.message))
+    axios
+      .get(URL)
+      .then((Response) => {
+              setLoading(true) 
+        // setData(Response.data)
+        console.log("Response.data", Response.data)
+        console.log("Response.data message", Response.data.message)
+        const res= Response.data.message
+        return res
+       })
+    
+      .then((res) => {
 
-      } catch (error) {
-        console.error(error.message)
-      }
-      setLoading(false)
-      // data.message !=null &&  data.message !="undefined" &&
-      //  setDogsArr(Object.keys(data.message))
-      // const dogsArr=Object.keys(data.message)
-    }
+         setDogsArr(Object.keys(res))
+        //  setDogsArr(Object.keys(data.message))
+        // console.log("dogsArr. ", dogsArr)
+        setLoading(false) 
 
-    fetchData()
+      })
+      .catch((error) => console.log(error))
   }, [])
 
-  //  function objToArr(data) {
-  //   const dogsArr=Object.keys(data.message)
-  //  }
+  // dogsArr && console.log("dogsArr", dogsArr)
+
+  // useEffect(() => {
+ 
+  //   const fetchDataDogs = async () => {
+  //     setLoading(true)
+  //     try {
+  //       const { data: response } = await axios.get(URL)
+  //         setData(response) 
+  //         console.log("response",response)
+  //         console.log("data",data)
+
+  //       //   if (data){
+  //       //     setDogsArr(Object.keys(data.message)) 
+  //       // }
+  //     } catch (error) {
+  //       console.error(error.message)
+  //     }
+  //     setLoading(false)
+  //     // data.message !=null &&  data.message !="undefined" &&
+  //     //  setDogsArr(Object.keys(data.message))
+  //     // const dogsArr=Object.keys(data.message)
+  //   }
+
+  //   fetchDataDogs()
+
+  // }, [])
+
+  // console.log("data2",data)
+
+  // function objToArr(data) {
+  //   const dogsArr = Object.keys(data.message)
+  // }
+
+  // const dogsArr = Object.keys(data.message)
 
   return (
     <div>
       {loading && <div>Loading</div>}
       {!loading && (
         <div>
-          {dogsArr.map((dog) => (
-            <div>
+          {dogsArr.map((dog,index) => (
+            <div key={index}>
               <p>{dog}</p>
-              <FetchImg dog={dog}/>
+              {/* <FetchImg dog={dog}/> */}
             </div>
           ))}
         </div>
